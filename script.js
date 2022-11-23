665de411fd6eeb777df47ce1b3ce5c613ed01e94
darkotto <82922249+darkotto@users.noreply.github.com>
2022-11-23 09:39:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import jsonData from './poi/poi.json' assert {type: "json"};
 
 window.onload = () => {
     const button = document.querySelector('button[data-action="change"]');
-    button.innerText = '10';
+    button.innerText = '11';
 
 //    let places = staticLoadPlaces();
 //    renderPlaces(places);
