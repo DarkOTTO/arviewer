568567ef34b863f33d10b3d174575f528378cadb
Otto Son <darkotto@hanmail.net>
2022-12-02 18:23:39 +0900

    Move shader code in the script.js file

--- assets/shader/vertex.glsl
+++ assets/shader/vertex.glsl
@@ -1,7 +1,6 @@
 #version 300 es
-uniform mat4 uModelMat;
-uniform mat4 uViewMat;
-uniform mat4 uProjMat;
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
 
 layout(location = 0) in vec3 aPosition;
 layout(location = 1) in vec2 aTexCoord;
@@ -12,15 +11,15 @@ out vec3 vNormal;
 out vec3 vVertex;
 
 vec4 projectPoint(in vec4 point) {
-   return uProjMat * point;
+   return projectionMatrix * point;
 }
 
 void main() {
-   mat4 normalMat = transpose(inverse(uViewMat * uModelMat));
+   mat4 normalMat = transpose(inverse(modelViewMatrix));
    vec4 normal = normalMat * vec4(aNormal, 0.0);
    vNormal = normalize(normal.xyz);
-   vVertex = (uViewMat * uModelMat * vec4(aPosition, 1.0)).xyz;
-   gl_Position = uProjMat * uViewMat * uModelMat * vec4(aPosition, 1.0);
+   vVertex = (modelViewMatrix * vec4(aPosition, 1.0)).xyz;
+   gl_Position = projectionMatrix * modelViewMatrix * vec4(aPosition, 1.0);
    vTexCoord = aTexCoord;
 }
 
