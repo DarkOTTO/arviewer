ba5dbb92049ccaf4b74852d92d2e3c5b5a35d43e
darkotto <82922249+darkotto@users.noreply.github.com>
2022-12-06 12:09:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -132,10 +132,10 @@ function renderPlaces() {
             '}'
         ].join('\n')
     });
-    model.setAttribute('obj-model', `obj: ${src}; mtl: ${mtl};`);
-//    model.setAttribute('obj-model', `obj: ${src};`);
-    model.setAttribute('material', `shader:custom_shader;`);
-//     model.setAttribute('material', `src: ${texture};`);
+//    model.setAttribute('obj-model', `obj: ${src}; mtl: ${mtl};`);
+    model.setAttribute('obj-model', `obj: ${src};`);
+//     model.setAttribute('material', `shader:custom_shader;`);
+    model.setAttribute('material', `src: ${texture};`);
     console.log("obj : " + src + ", texture : " + texture);
     model.setAttribute('scale', '1.2 1.2 1.2');
     model.setAttribute('rotation', '0 0 0');
