706710e9829ed593c4df54ca57d763e7892a6ec5
Otto Son <darkotto@hanmail.net>
2022-12-15 15:56:33 +0900

    Modify dropdown menu code

--- script.js
+++ script.js
@@ -28,6 +28,23 @@ window.onload = () => {
 //     }
 // });
 
+function dropdownFunction() {
+	document.getElementById("objs").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
 window.addEventListener("load", () => {
     var dropBtn = document.getElementById('SelectOBJ');
     var dropDetailsBanner = document.getElementById('banner');
